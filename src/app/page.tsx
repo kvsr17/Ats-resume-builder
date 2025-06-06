b2fd54d4bb2d96661cdfa734453b534aed7c4420
Venkata Sridhar K <kvenkatasridhar0@gmail.com>
2025-06-06 15:51:36 +0000

    when i click download it should only show resume preview section only\

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import PageHeader from '@/components/PageHeader';
 import ResumeEditor from '@/components/resume/ResumeEditor';
 import ResumePreview from '@/components/resume/ResumePreview';
@@ -7,9 +8,9 @@ import { ResumeProvider } from '@/contexts/ResumeContext';
 export default function Home() {
   return (
     <ResumeProvider>
-      <div className="flex flex-col min-h-screen bg-background">
+      <div className="flex flex-col min-h-screen bg-background print:bg-transparent">
         <PageHeader />
-        <main className="flex-grow container mx-auto px-4 py-6">
+        <main className="flex-grow container mx-auto px-4 py-6 print:p-0 print:m-0 print:max-w-none">
           <ResumeToolbar />
           <div className="flex flex-col lg:flex-row gap-6 h-[calc(100vh-200px)] lg:h-[calc(100vh-220px)] no-print">
             <div className="lg:w-1/2 h-full">
@@ -19,7 +20,7 @@ export default function Home() {
               <ResumePreview />
             </div>
           </div>
-          {/* Hidden preview for printing, if different layout is needed or to ensure only preview prints */}
+          {/* Hidden preview for printing, to ensure only preview prints and is correctly formatted */}
           <div className="hidden print:block">
              <ResumePreview />
           </div>
