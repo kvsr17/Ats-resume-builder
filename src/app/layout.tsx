0d384ef6d0fd054a018115d90e726e4beecab127
Venkata Sridhar K <kvenkatasridhar0@gmail.com>
2025-06-06 15:57:00 +0000

    When I click download the preview looks like this. I don't want ats resu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />
+        <div className="no-print print:hidden">
+          <Toaster />
+        </div>
       </body>
     </html>
   );
